b0217bb9119ed3d53b1a65c885adcf684b3894dc
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-03 00:18:30 +0600

    for-of

---
+++ for-of.js
@@ -0,0 +1,27 @@
+const numbers = [12, 98, 45, 63, 21, 72];
+console.log(numbers);
+for (const num of numbers) {
+  console.log(num);
+}
+// for >> of
+//  for ( this variable of that variable) { console.log ( this variable)}
+
+// output
+// 12
+// 98
+// 45
+// 63
+// 21
+// 72
+
+const fruits = ["orange", "apple", "banana", "jackfruit", "watermelons"];
+for (const fruit of fruits) {
+  console.log(fruit);
+  console.log("I want to eat");
+}
+// output
+// orange
+// apple
+// banana
+// jackfruit
+// watermelons
