8b6682c89fc8ec3253cee4041cea11b800220856
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-04 01:25:30 +0600

    break and continue

---
+++ break.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 15; i++) {
+  console.log(i);
+  if (i >= 5) {
+    break;
+  }
+}
+
+let i = 54;
+while (i > 25) {
+  if (i < 50) {
+    break;
+  }
+  console.log(i);
+  i--;
+}
---
+++ continue.js
@@ -0,0 +1,18 @@
+// - Break —> I am done with this loop. Loop end.
+// - continue —> skip this one
+
+for (let i = 0; i < 10; i++) {
+  if (i % 2 === 1) {
+    continue;
+  }
+  console.log(i);
+}
+
+let i = 0;
+while (i < 50) {
+  i++;
+  if (i % 5 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
