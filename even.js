b282d130d69d104081474d2235b33625b87451ba
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-04 00:33:38 +0600

    for

--- even.js
+++ even.js
@@ -1,9 +1,7 @@
 let num = 1;
 while (num <= 10) {
-  console.log(num);
-
   if (num % 2 === 0) {
-    console.log(num, "even number");
+    console.log(num);
   }
   num++;
 }
---
+++ for.js
@@ -0,0 +1,14 @@
+let num = 0; // loop variable initialization
+while (num < 5) {
+  // condition to continue the loop
+  console.log(num); // loop statement
+  num++; // loop variable change
+}
+
+// for ( first part ; second part  third part) {
+//     loop statement
+// }
+
+for (let num = 0; num < 5; num++) {
+  console.log(num);
+}
