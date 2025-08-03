126d515fbfb36a14081f11359096c0a17f619f2a
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-04 02:52:08 +0600

    for tasks

--- for_tasks.js
+++ for_tasks.js
@@ -1,58 +1,73 @@
 /***
-
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+*/
 
- */
+for (let i = 1; i <= 60; i++) {
+  console.log(
+    "I will invest at least 6 hrs every single day for next 60 days!",
+    i
+  );
+}
 
-/*programming hero*/
-
-/***
-
-Subtask-1:
-
+/***Subtask-1:
 Find all the odd numbers from 61 to 100.
+*/
 
- */
+for (let ii = 61; ii <= 100; ii++) {
+  if (ii % 2 === 1) {
+    console.log(ii);
+  }
+}
 
 /***
-
 Subtask-2:
-
 Find all the even numbers from 78 to 98.
+*/
 
- */
-
-/*programming hero*/
+for (let jj = 78; jj <= 98; jj++) {
+  if (jj % 2 === 0) {
+    console.log(jj);
+  }
+}
 
 /***
-
 Subtask-1:
-
 Display sum of all the odd numbers from 91 to 129.
+*/
 
- */
+let sumOdd = 0;
+for (let kk = 91; kk <= 129; kk++) {
+  if (kk % 2 === 1) {
+    sumOdd = sumOdd + kk;
+  }
+}
+console.log("Total sum of odd numbers from 91 to 129 is", sumOdd);
+
 /***
-
 Subtask-2:
-
 Display sum of all the even numbers from 51 to 85.
+*/
 
- */
-
-/*programming hero*/
+let sumEven = 0;
+for (let ll = 51; ll <= 85; ll++) {
+  if (ll % 2 === 0) {
+    sumEven = sumEven + ll;
+  }
+}
+console.log("Total sum of even numbers from 51 to 85 is", sumEven);
 
 /***
-
 Generate a multiplication table for number 9
+*/
 
- */
-
-/*programming hero*/
+for (let mm = 1; mm <= 10; mm++) {
+  console.log("9 x", mm, "=", 9 * mm);
+}
 
 /***
-
 Implement a countdown timer that counts down from 81 to 65.
+*/
 
- */
-
-/*programming hero*/
+for (let nn = 81; nn >= 65; nn--) {
+  console.log(nn);
+}
--- while_tasks.js
+++ while_tasks.js
@@ -1,36 +1,55 @@
 /***
-
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+*/
 
- */
-
-/*programming hero*/
+let i = 1;
+while (i <= 60) {
+  console.log(
+    "I will invest at least 6 hrs every single day for next 60 days!",
+    i
+  );
+  i++;
+}
 
 /***
-
 Subtask-1:
-
 Find all the odd numbers from 61 to 100.
+*/
 
- */
+let ii = 61;
+while (ii <= 100) {
+  if (ii % 2 === 1) {
+    console.log(ii);
+  }
+  ii++;
+}
 
-/***
+// Subtask-2:
+// Find all the even numbers from 78 to 98.
 
-Subtask-2:
+let jj = 78;
+while (jj <= 98) {
+  if (jj % 2 === 0) {
+    console.log(jj);
+  }
+  jj++;
+}
 
-Find all the even numbers from 78 to 98.
+// Subtask-1:
 
- */
+// Display sum of all the odd numbers from 81 to 131.
 
-/*programming hero*/
+//  */
 
-/***
-
-Subtask-1:
-
-Display sum of all the odd numbers from 81 to 131.
-
- */
+let kk = 81;
+let sumOdd = 0;
+while (kk <= 131) {
+  if (kk % 2 === 1) {
+    sumOdd = sumOdd + kk;
+  }
+  kk++;
+}
+console.log("Total sum of odd numbers from 81 to 131 is", sumOdd);
 /***
 
 Subtask-2:
@@ -39,7 +58,15 @@ Display sum of all the even numbers from 206 to 311.
 
  */
 
-/*programming hero*/
+let ll = 206;
+let sumEven = 0;
+while (ll <= 311) {
+  if (ll % 2 === 0) {
+    sumEven = sumEven + ll;
+  }
+  ll++;
+}
+console.log("Total sum of even numbers from 206 to 311 is", sumEven);
 
 /***
 
@@ -47,7 +74,11 @@ As Era is learning now, she wants to explore more and more. Tell Era to generate
 
  */
 
-/*programming hero*/
+let mm = 1;
+while (mm <= 10) {
+  console.log("5 x", mm, "=", 5 * mm);
+  mm++;
+}
 
 /***
 
@@ -55,4 +86,8 @@ Implement a countdown timer that counts down from 21 to 15.
 
  */
 
-/*programming hero*/
+let nn = 21;
+while (nn >= 15) {
+  console.log(nn);
+  nn--;
+}
