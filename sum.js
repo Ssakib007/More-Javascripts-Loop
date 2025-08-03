2dd7b8a06a30c44c3718ee5b6e5fd6c5fdae86bc
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-03 23:59:27 +0600

    while

---
+++ sum.js
@@ -0,0 +1,9 @@
+let numb = 1;
+let sum = 0;
+while (numb <= 10) {
+  console.log(numb);
+  sum = sum + numb;
+  console.log("sum is", sum);
+  numb++;
+}
+console.log("Total sum is", sum);
---
+++ while.js
@@ -0,0 +1,15 @@
+const number = 7;
+if (number < 10) {
+  console.log("small number");
+}
+console.log("something new is cooking");
+
+// while loop
+let num = 0;
+
+while (num < 5) {
+  console.log("looping", num);
+  // num = num + 1;
+  // num += 1;
+  num++;
+}
